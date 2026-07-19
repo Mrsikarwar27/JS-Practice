c7d41adf8e6e5d4721a2b0a580804d6e4e032605
Mrsikarwar27 <nabhsikarwar@gmail.com>
2026-07-19 22:50:30 +0530

    feat: add index.js with commented-out array filtering and currying examples

---
+++ index.js
@@ -0,0 +1,28 @@
+
+// const arr = [1, 2, 3, "a", "fd"];
+// const char = [];
+// const num = [];
+
+// arr.forEach((element) => {
+//   if (typeof element === "string") {
+//     char.push(element);
+//   } else {
+//     num.push(element);
+//   }
+// });
+
+// console.log(char);
+// console.log(num);
+
+//===========================================================
+// function curry(a) {
+//   return function (b) {
+//     return function (c) {
+//       console.log(a + b + c);
+//     };
+//   };
+// }
+
+// curry(2)(3)(1);
+
+//===========================================================
